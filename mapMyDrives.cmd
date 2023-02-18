1384365896219ef0f470987d464ee9659beeba74
paroxy777 <65880457+paroxy777@users.noreply.github.com>
2023-02-18 02:24:30 -0500

    Version 0
    First upload. 
    
    mapMyDrives.cmd
    Current Features
    ===========================================
    1. Maps my personal network drives and prompts for SMB credentials. 
    2. UNC's are hard coded in. 
    3. The 'Remove Mapping' function indiscriminately removes ALL existing mapped drives.
    
    Future Features
    ===========================================
    1. Error handling for failed commands.
    2. MY DRIVES: Ability to add/edit/manage/remove UNC's from lists of servers w. shares, and usernames. Store in a flat file DB maybe? XML? JSON? CSV?
    3. Windows doesn't allow multiple connections to a server using DIFFERENT usernames, enforce this. Parse existing mappings?

---
+++ mapMyDrives.cmd
@@ -0,0 +1,90 @@
+@ECHO OFF
+CLS
+SET "scriptTitle=    MAP MY DRIVES"
+SET barDouble=====================================================================
+SET barSingle=--------------------------------------------------------------------
+SET mapHome=0
+SET mapWTC=0
+SET clearMaps=0
+SET smbUser=0
+SET smbPass=0
+
+:: Display HEADER text
+ECHO %barDouble%
+ECHO %scriptTitle%
+ECHO %barDouble%
+
+ECHO -- YOUR EXISTING MAPPED DRIVES:
+ECHO %barSingle%
+NET USE
+
+
+:: Choices, choices, choices.
+ECHO %barSingle%
+ECHO -- What are we doing today?
+ECHO %barSingle%
+CHOICE /C YN /M "Remove all exisiting mapped drives?"
+IF %ERRORLEVEL% EQU 1 SET clearMaps=1
+CHOICE /C YN /M "Map drives for Home Server 10.77.80.50?"
+IF %ERRORLEVEL% EQU 1 SET mapHome=1
+CHOICE /C YN /M "Map drives for WTC Server 192.168.10.10?"
+SET mapWTC=%ERRORLEVEL%
+
+:: MAIN APP BODY
+IF %clearMaps% EQU 1 (CALL :sub_clear_exisiting) ELSE (ECHO --Keeping current mapped drives.)
+IF %clearMaps% EQU 1 (CALL :sub_map_home) ELSE (ECHO --Mapping drives for Home Server 10.77.80.50.)
+IF %clearMaps% EQU 1 (CALL :sub_map_wtc) ELSE (ECHO --Mapping drives for WTC Server 192.168.1.10.)
+GOTO eof
+
+:: SUB-ROUTINES
+:sub_clear_exisiting
+ECHO %barSingle%
+ECHO -- REMOVING CURRENTLY MAPPED DRIVES
+ECHO %barSingle%
+NET USE * /delete
+EXIT /B
+
+:sub_map_home
+ECHO %barSingle%
+ECHO -- MAPPING DRIVES FOR HOME SERVER
+ECHO %barSingle%
+SET /P smbUser=Enter SMB username on [10.77.80.50]: 
+SET /P smbPass=Enter SMB password for [%smbUser%] on [10.77.80.50]: 
+ECHO --- H: for "Downloads"
+NET USE H: \\10.77.80.50\Downloads %smbPass% /user:%smbUser% /persistent:yes <NUL
+SET "smbPass="
+ECHO --- I: for "David Stuff"
+NET USE I: "\\10.77.80.50\David Stuff" /persistent:yes <NUL
+ECHO --- J: for "isos"
+NET USE J:  \\10.77.80.50\isos /persistent:yes <NUL
+EXIT /B
+
+:sub_map_wtc
+ECHO %barSingle%
+ECHO -- MAPPING DRIVES FOR WTC
+ECHO %barSingle%
+SET /P smbUser=Enter SMB username on [192.168.1.10]: 
+SET /P smbPass=Enter SMB password for [%smbUser%] on [192.168.1.10]: 
+ECHO --- S: for "isos"
+NET USE S: \\192.168.1.10\isos %smbPass% /user:%smbUser% /persistent:yes <NUL
+SET "smbPass="
+ECHO --- T: for "Tools"
+NET USE T: \\192.168.1.10\Tools /persistent:yes <NUL
+EXIT /B
+
+:: CLEAN-UP AND EXIT
+:eof
+REM Show results to user
+ECHO %barSingle%
+ECHO -- YOUR MAPPED DRIVES:
+ECHO %barSingle%
+NET USE
+ECHO -- DONE
+REM Clear VARs
+SET "scriptTitle="
+SET "barDouble="
+SET "barSingle="
+SET "mapHome="
+SET "mapWTC="
+SET "clearMaps="
+SET "smbPass="
